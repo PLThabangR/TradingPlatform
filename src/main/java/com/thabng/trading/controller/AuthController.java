be0e687cd8f938ff5d5d0d36d8b8d698e6d60872
plthabangr <rakgoropothabang@gmail.com>
2025-05-07 12:02:34 +0200

    Created the rest controller to register new user to the data base

---
+++ src/main/java/com/thabng/trading/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.thabng.trading.controller;
+
+
+import com.thabng.trading.Repository.UserRepository;
+import com.thabng.trading.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+     @Autowired
+    private UserRepository userRepository;
+
+     @PostMapping("/signup")
+     public User registerUser(@RequestBody User user){
+
+//         //Create new user
+//         User newUser = new User();
+//
+//         newUser.setEmail(user.getEmail());
+//         newUser.setFullName(user.getFullName());
+//         newUser.setPassword(newUser.getPassword());
+//         newUser.setRole(newUser.getRole());
+
+         return userRepository.save(user);
+     }
+}
